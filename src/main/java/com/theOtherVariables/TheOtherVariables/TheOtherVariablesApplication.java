0e7017cf3c8f6e7965396d9640d6e385873fee60
Jennifer Obregon <jenniem579@yahoo.com>
2019-04-29 10:19:36 -0500

    initial commit;

---
+++ src/main/java/com/theOtherVariables/TheOtherVariables/TheOtherVariablesApplication.java
@@ -0,0 +1,13 @@
+package com.theOtherVariables.TheOtherVariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOtherVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheOtherVariablesApplication.class, args);
+	}
+
+}
